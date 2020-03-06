e003b84a515d9e63260c6c5dea36a7b876ac9307
Guyutongxue <guyutongxue@163.com>
2020-03-06 18:57:14 +0800

    part of 0306

---
+++ 20200306Inherit/B.DerivedString.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+class MyString : public string {
+public:
+    MyString(const char* s) : string(s) {}
+    MyString(string s) : MyString(s.c_str()) {}
+    MyString() {}
+    string operator()(int pos, int len) {
+        return substr(pos, len);
+    }
+};
+
+int main() {
+    MyString s1("abcd-"), s2, s3("efgh-"), s4(s1);
+    MyString SArray[4] = {"big", "me", "about", "take"};
+    cout << "1. " << s1 << s2 << s3 << s4 << endl;
+    s4 = s3;
+    s3 = s1 + s3;
+    cout << "2. " << s1 << endl;
+    cout << "3. " << s2 << endl;
+    cout << "4. " << s3 << endl;
+    cout << "5. " << s4 << endl;
+    cout << "6. " << s1[2] << endl;
+    s2 = s1;
+    s1 = "ijkl-";
+    s1[2] = 'A';
+    cout << "7. " << s2 << endl;
+    cout << "8. " << s1 << endl;
+    s1 += "mnop";
+    cout << "9. " << s1 << endl;
+    s4 = "qrst-" + s2;
+    cout << "10. " << s4 << endl;
+    s1 = s2 + s4 + " uvw " + "xyz";
+    cout << "11. " << s1 << endl;
+    sort(SArray, SArray + 4);
+    for (int i = 0; i < 4; i++)
+        cout << SArray[i] << endl;
+    // s1的从下标0开始长度为4的子串
+    cout << s1(0, 4) << endl;
+    // s1的从下标5开始长度为10的子串
+    cout << s1(5, 10) << endl;
+    return 0;
+}
